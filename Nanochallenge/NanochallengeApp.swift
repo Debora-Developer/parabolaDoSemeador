3fea5d8665c954d96e42ffe390851fa6a99180f1
Débora <deboracosta.designer@gmail.com>
2025-07-30 08:59:59 -0300

    Initial Commit

---
+++ Nanochallenge/NanochallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NanochallengeApp.swift
+//  Nanochallenge
+//
+//  Created by Débora Costa on 30/07/25.
+//
+
+import SwiftUI
+
+@main
+struct NanochallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
